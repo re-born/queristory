76dcceecd65aa085c40779f42897f7657eba6e9b
daichi <daipanchi@gmail.com>
2014-07-01 03:47:06 +0900

    Enable link to home

--- rails/config/routes.rb
+++ rails/config/routes.rb
@@ -2,6 +2,7 @@ ShameHistory::Application.routes.draw do
   resources :queries
   post 'query/create' => 'queries#create'
   post 'page/create' => 'pages#create'
+  root 'queries#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
