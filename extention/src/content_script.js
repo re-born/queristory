26d844924e9216d54384a7c8e57ccd715dd8b394
daichi <daipanchi@gmail.com>
2014-06-30 16:29:02 +0900

    Avoid duplicated POST

--- extention/src/content_script.js
+++ extention/src/content_script.js
@@ -2,6 +2,8 @@ chrome.runtime.onMessage.addListener(function(msg, sender, sendResponse) {
 
 })
 
+var latest_url = ''
+
 function add_binding_to_page() {
   $.each($('.g h3 a[onmousedown]'), function(i, dom) {
     $(this).click(function () {
@@ -14,11 +16,14 @@ function add_binding_to_page() {
         var query = parsed_url.hash.slice(1)
       }
       var page_data = query + '&url=' + clicked_url + '&rank=' + i
-      $.ajax({
-        url: 'http://0.0.0.0:1984/page/create',
-        type: 'post',
-        data: page_data,
-      })
+      if(clicked_url != latest_url){
+        $.ajax({
+          url: 'http://0.0.0.0:1984/page/create',
+          type: 'post',
+          data: page_data,
+        })
+        latest_url = clicked_url
+      }
     })
   })
 }
