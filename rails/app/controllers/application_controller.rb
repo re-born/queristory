6849f969b29bc91ec99fa88985e0e1d08f9a5206
daichi <daipanchi@gmail.com>
2014-07-07 02:43:16 +0900

    Activate caching

--- rails/app/controllers/application_controller.rb
+++ rails/app/controllers/application_controller.rb
@@ -3,5 +3,11 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   # protect_from_forgery with: :exception
   protect_from_forgery with: :null_session
+  before_filter :profile_for_admins
 
+  def profile_for_admins
+    if Rails.env.development? || current_user.try(:staff?)
+      Rack::MiniProfiler.authorize_request
+    end
+  end
 end
