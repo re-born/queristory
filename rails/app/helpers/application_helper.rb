0c493f493788e3f61b8587776555eaf59f617fcf
daichi <daipanchi@gmail.com>
2014-11-20 19:02:01 +0900

    Add queristory_from to view

--- rails/app/helpers/application_helper.rb
+++ rails/app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+  GOOGLE_SEARCH_URL = 'http://google.com/search'
+
+  def url_for_search(query, option)
+    url = "#{GOOGLE_SEARCH_URL}?q=#{query.q}"
+
+    if option
+      url += "&#{option}=#{query[option]}"
+    end
+    "#{url}&queristory_from=#{query.id}"
+  end
 end
