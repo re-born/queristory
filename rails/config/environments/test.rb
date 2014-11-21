5f4062a80da545db185e6804d3561ceb61225446
daichi <daipanchi@gmail.com>
2014-11-21 18:09:41 +0900

    Change app name

--- rails/config/environments/test.rb
+++ rails/config/environments/test.rb
@@ -1,4 +1,4 @@
-ShameHistory::Application.configure do
+Queristory::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
   # The test environment is used exclusively to run your application's
