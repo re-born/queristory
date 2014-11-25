19e0b355aca412bf5160e6d7fbe579ad62c53cd4
daichi <daipanchi@gmail.com>
2014-11-26 04:51:47 +0900

    Refactor options.js

--- extention/src/options.js
+++ extention/src/options.js
@@ -1,57 +1,51 @@
 document.body.onload = function() {
   chrome.storage.sync.get('teams_source_url', function(item) {
     $('#teams_list_source').val(item.teams_source_url)
-    get_teams_list()
+    reload_teams_list()
   })
   chrome.storage.sync.get('team_info', function(item) {
     $('#team_password').val(item.team_info.team_password)
   })
 }
 
-$('#team_info').submit(function(){
+$('#register_team_info').submit(function(){
   event.preventDefault();
-  set_team_info()
+  save_team_info_to_storage()
   check_auth()
 })
 
 $('#get_teams_list').submit(function(){
   event.preventDefault();
-  get_teams_list()
-  set_teams_source_url()
+  reload_teams_list()
+  save_teams_source_url_to_strage()
 })
 
-function set_teams_source_url() {
+function save_teams_source_url_to_strage() {
   chrome.storage.sync.set({
     teams_source_url: $('#teams_list_source').val()
   })
 }
 
-function set_team_info() {
+function save_team_info_to_storage() {
   chrome.storage.sync.set({
-    team_info: {
-      team_name: $('#teams_list').val(),
-      team_password: $('#team_password').val(),
-    }
+    team_info: get_team_info_for_auth()
   })
 }
 
-function get_teams_list() {
+function reload_teams_list() {
   $('#teams_list > option').remove();
-  $.ajax({
-    url: $('#teams_list_source').val() + '/teams',
-    type: 'get',
-    headers: {
-      'X-HTTP-Method-Override': 'get',
-      'Content-Type': 'application/json'
-    },
-    dataType: 'json'
-  }).done(function(res) {
-    $.each(res, function(i, team_name) {
+
+  var teams_source_url = $('#teams_list_source').val() + '/teams';
+  $.getJSON( teams_source_url, function(teams) {
+    $.each(teams, function(i, team_name) {
       $('#teams_list').append($('<option>').html(team_name).val(team_name));
     })
+
+    // focus the team which has already selected
     chrome.storage.sync.get('team_info', function(item) {
       $('#teams_list').val(item.team_info.team_name)
     })
+
     if ( $('#team_password').val() != '' ) {
       check_auth() // passが入力されているときに認証確認
     }
@@ -59,23 +53,16 @@ function get_teams_list() {
 }
 
 function check_auth() {
-  var team_info_for_auth = {
+  var auth_url = $('#teams_list_source').val() + '/auth'
+  $.getJSON( auth_url, get_team_info_for_auth(), function(res) {
+    var message = res ? 'Queristory is now active!' : 'failed';
+    $('#auth_success').text(message)
+  })
+}
+
+function get_team_info_for_auth() {
+  return {
     team_name: $('#teams_list').val(),
     team_password: $('#team_password').val(),
   }
-  $.ajax({
-    url: $('#teams_list_source').val() + '/auth',
-    type: 'get',
-    headers: {
-      'X-HTTP-Method-Override': 'get',
-      'Content-Type': 'application/json'
-    },
-    dataType: 'json',
-    data: team_info_for_auth,
-  }).done(function(res) {
-    if (res)
-      $('#auth_success').text('Queristory is now active!!')
-    else
-      $('#auth_success').text('something went wrong...')
-  })
 }
