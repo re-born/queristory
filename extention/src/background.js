959d2e002b9a1a1dcacd51f2f914002b71a117f3
daichi <daipanchi@gmail.com>
2014-11-26 21:24:53 +0900

    Post query with authentification from chrome.storage

--- extention/src/background.js
+++ extention/src/background.js
@@ -1,12 +1,12 @@
-const SESSION_INTERVAL = 600000
+const SESSION_INTERVAL = 10 * 60 * 1000
 var last_query = ''
 var latest_session_date = new Date()
 var session_id = generate_session_id()
 
-function post_query(query, session_id) {
+function post_query(query, session_id, auth_info) {
   query = query + '&session_id=' + session_id
-  query = query + '&team_name=' + 'sakai_lab'
-  query = query + '&team_password=' + 'pass_hoge'
+  query = query + '&team_name=' + auth_info.team_name
+  query = query + '&team_password=' + auth_info.team_password
   if(query != last_query){
     $.ajax({
       url: 'http://0.0.0.0:1984/query/create',
@@ -27,18 +27,14 @@ function check_session_id() {
 
 chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
   if (changeInfo.status == 'complete') {
-    parsed_url = parseUrl(tab.url)
+    var parsed_url = parseUrl(tab.url)
     hostname_condition = parsed_url.hostname == 'www.google.co.jp'
                       || parsed_url.hostname == 'www.google.com'
     pathname_condition = parsed_url.pathname == '/search'
                       || parsed_url.pathname == '/webhp'
 
     if (hostname_condition && pathname_condition){
-      if (parsed_url.hash == '') {
-        post_query(parsed_url.search.slice(1), check_session_id())
-      } else {
-        post_query(parsed_url.hash.slice(1), check_session_id())
-      }
+      post_query_with_auth(parsed_url)
     }
   }
 })
@@ -51,3 +47,14 @@ chrome.extension.onRequest.addListener(
       sendResponse({}) // snub them.
   }
 )
+
+function post_query_with_auth(parsed_url) {
+  chrome.storage.sync.get('team_info', function(item) {
+    var auth_info = { team_name: item.team_info.team_name,
+                  team_password: item.team_info.team_password }
+
+    var query = parsed_url.hash == '' ? parsed_url.search.slice(1)
+                                      : parsed_url.hash.slice(1)
+    post_query(query, check_session_id(), auth_info)
+  })
+}
