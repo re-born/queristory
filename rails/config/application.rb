932377f3d891ef1268cb9e7ebb9e8491f56d7d43
daichi <daipanchi@gmail.com>
2014-12-11 07:31:27 +0900

    Create prototype of getting trend words

--- rails/config/application.rb
+++ rails/config/application.rb
@@ -18,6 +18,7 @@ module Queristory
     config.time_zone = 'Tokyo'
 
     config.assets.precompile << /(^[^_\/]|\/[^_])[^\/]*$/
+    config.autoload_paths += %W(#{Rails.root}/lib)
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
